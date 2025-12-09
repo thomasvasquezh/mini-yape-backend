a02ae80390b873884b4848a5ae6a01d3b87ad870
thomasvasquezh <tom080901@hotmail.com>
2025-12-08 21:31:47 -0500

    initial project

---
+++ src/main/java/com/project/miniyape/MiniYapeApplication.java
@@ -0,0 +1,13 @@
+package com.project.miniyape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniYapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniYapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/miniyape/MiniYapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.miniyape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniYapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
